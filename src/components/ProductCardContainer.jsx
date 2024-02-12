69e02df88099256d100edd64ddb2b6fac8c13481
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-02-12 15:28:20 -0500

    remove curly braces

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,22 +1,24 @@
 
 const productCard = () => {
-  return 
-    <div className="product-card">
-      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <div className="price">${product.price}</div>
+  return (
+      <img src="https://multimedia.bbycastatic.ca/multimedia/products/500x500/169/16982/16982680.jpg" 
+      alt="hp 15" />
+      <h3>HP 15" Laptop w/ 1 year of Microsoft 365 - Natural Silver (Intel N100/128GB SSD/4GB RAM)</h3>
+      <p>1.0GHz Intel N100 quad-core CPU and 4GB DDR4 RAM can power productivity tasks as well as leisurely tasks including creating presentations, watching videos, and casual 
+        photo editing 128GB UFS storage provides a safe and reliable storage for your important documents and files</p>
+      <div className="price">
+        $487.79
+       </div>
+
       <button>Add to Cart</button>
-    </div>
-  );
+  
 };
 
 
 
 function ProductCardContainer() {
+    //Add three product cards here
     return (
         <h1>Discover Your Favorites</h1>
     )
 }
-
-export ProductCardContainer

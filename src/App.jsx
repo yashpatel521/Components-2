8c9da999a99bfafaf146a02a02128da66beb3ddc
Shalom Steinmetz <shalomsteinmetz@gmail.com>
2024-02-19 12:21:26 -0500

    fix formating

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ProductCardContainer from "./components/ProductCardContainer";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <main className="store">

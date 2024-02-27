ee50ea83c2adb6f10dae00ee52da1ab662a7fcf0
Yash Patel <yash1451999@gmail.com>
2024-02-27 17:32:47 -0500

    assigment 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import ProductCardContainer from "./components/ProductCardContainer";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { user } from "./constant/User";
+import StyleWrapper from "./components/StyleWrapper";
+
+const styles = {
+  backgroundColor: "#f8f9fa",
+  padding: "20px",
+  textAlign: "center",
+  borderTop: "1px solid #dee2e6",
+  boxShadow: "0 -2px 4px rgba(0, 0, 0, 0.1)",
+};
 
 function App() {
   return (
     <main className="store">
       <NavBar user={user} />
       <ProductCardContainer />
-      <Footer />
+      <StyleWrapper styles={styles} section={<Footer />} />
     </main>
   );
 }
